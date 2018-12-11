6381539d7a38b728280423a67fa448e3cd00a5d2
Malte Kölle <malte.koelle@unic.com>
2018-12-11 12:20:35 +0100

    Add some Sysouts

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,12 +9,14 @@ public class CalculatorTest {
     @Test
     public void testSummeZweiIstOk(){
         testee = new Calculator();
+        System.out.println("First test");
         assertTrue(testee.summe(10, 25) == 35) ;
     }
 
     @Test
     public void testSubtraktionZweiIstOk(){
         testee = new Calculator();
+        System.out.println("Second test");
         assertTrue(testee.subtraktion(25, 10) == 15);
     }
 }
